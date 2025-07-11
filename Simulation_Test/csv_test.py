51e1099e900c5fe958ce8c0c8255abd4315777a6
Matthewsshev <matthewsshev@gmail.com>
2025-07-11 12:12:58 +0200

    adding args to csv_test

--- Simulation_Test/csv_test.py
+++ Simulation_Test/csv_test.py
@@ -1,7 +1,7 @@
 import csv
-
+import argparse
 def read_csv(path):
-    with open(f'Simulation_Temp/{path}', newline='', encoding='utf-8') as f:
+    with open(f'Simulation_Temp/{path}.csv', newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         # Convert each row to a tuple of key-value pairs, sorted by key
         return sorted(tuple(sorted(row.items())) for row in reader)
@@ -15,10 +15,19 @@ def compare_files(file1, file2):
     else:
         return False
 
+def parse_args():
+    # Define argument parser
+    parser = argparse.ArgumentParser()
+    # Add argument for standard file
+    parser.add_argument('-s', type=str, default='modular_test')
+    # Add argument for new modified file, that needs to be compared
+    parser.add_argument('-m', type=str, default='test_changes')
+    return parser.parse_args()
 
 def main():
-    new_file_for_comparison = 'modular_test.csv'
-    old_file_to_compare_with = 'test_changes.csv'
+    args = parse_args()
+    new_file_for_comparison = args.m
+    old_file_to_compare_with = args.s
 
     result = compare_files(new_file_for_comparison, old_file_to_compare_with)
     if result:
